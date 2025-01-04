d562509b4edf2c377f7c99a7cf1eb42a5f349641
ShadowGTP <114325926+ShadowGTP@users.noreply.github.com>
2025-01-04 15:51:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.all('/player/login/dashboard', function (req, res) {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n');
         const uName = uData[0].split('|');
         const uPass = uData[1].split('|');
+        
         for (let i = 0; i < uData.length - 1; i++) {
             const d = uData[i].split('|');
             tData[d[0]] = d[1];
